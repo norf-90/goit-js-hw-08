1af1f7c6cb1d416121279ab55dbe27e54f60ea31
Oleksandr Shmatko <blacknorfolk@gmail.com>
2022-12-23 21:02:34 +0200

    add SimpleLightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,29 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+console.log(SimpleLightbox);
+
+const galleryEl = document.querySelector('.gallery');
+generateMarkup(galleryItems, galleryEl);
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+function generateMarkup(galleryItems, element) {
+  const markup = galleryItems
+    .map(
+      ({ original, preview, description }) =>
+        `<li><a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" 
+        alt="${description}" />
+      </a></li>`
+    )
+    .join('');
+
+  element.insertAdjacentHTML('beforeend', markup);
+}
