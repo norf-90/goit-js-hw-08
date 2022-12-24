9363179678addfa758f2a931f79b8d2c0666d496
Oleksandr Shmatko <blacknorfolk@gmail.com>
2022-12-25 00:26:39 +0200

    add vimeo player

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,17 +1,14 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
-
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-console.log(SimpleLightbox);
+import { galleryItems } from './gallery-items';
+// Change code below this line
 
 const galleryEl = document.querySelector('.gallery');
 generateMarkup(galleryItems, galleryEl);
 
 var lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
-  captionPosition: 'bottom',
   captionDelay: 250,
 });
 
@@ -19,10 +16,10 @@ function generateMarkup(galleryItems, element) {
   const markup = galleryItems
     .map(
       ({ original, preview, description }) =>
-        `<li><a class="gallery__item" href="${original}">
+        `<a class="gallery__item" href="${original}">
         <img class="gallery__image" src="${preview}" 
         alt="${description}" />
-      </a></li>`
+      </a>`
     )
     .join('');
 
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
 
+const KEY_NAME = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+player.setCurrentTime(localStorage[KEY_NAME]);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(event) {
+  localStorage[KEY_NAME] = event.seconds;
+  console.log(event.percent);
+}
