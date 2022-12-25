c3bcaa5ed694d01749b460acae6b01b7f66c8b36
Oleksandr Shmatko <blacknorfolk@gmail.com>
2022-12-26 01:25:50 +0200

    add form logic

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,17 @@
 import Player from '@vimeo/player';
-var throttle = require('lodash.throttle');
+import throttle from 'lodash.throttle';
 
 const KEY_NAME = 'videoplayer-current-time';
 
 const iframe = document.querySelector('iframe');
-const player = new Player(iframe);
-player.setCurrentTime(localStorage[KEY_NAME]);
+const player = new Player(iframe, { id: 19231868, width: 640 });
+
+const time = localStorage[KEY_NAME] ?? 0;
+player.setCurrentTime(time);
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
-function onPlay(event) {
-  localStorage[KEY_NAME] = event.seconds;
-  console.log(event.percent);
+function onPlay(currentTime) {
+  localStorage[KEY_NAME] = currentTime.seconds;
+  console.log(currentTime.seconds);
 }
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+import throttle from 'lodash.throttle';
 
+const KEY_NAME = 'feedback-form-state';
+const formEl = document.querySelector('.feedback-form');
+const inputEmailEl = document.querySelector('[name=email]');
+const inputMessageEl = document.querySelector('[name=message]');
+console.log(inputEmail);
+const buttonEl = document.querySelector('[type=submit]');
+const formData = {};
+
+inputEmailEl.value = load(KEY_NAME).email ?? '';
+inputMessageEl.value = load(KEY_NAME).message ?? '';
+
+formEl.addEventListener('input', throttle(onFormInput, 500));
+buttonEl.addEventListener('click', onButtonClick);
+
+function onFormInput(event) {
+  formData[event.target.name] = event.target.value;
+  save(KEY_NAME, formData);
+}
+
+function onButtonClick(event) {
+  event.preventDefault();
+  console.log(load(KEY_NAME));
+  localStorage.clear();
+  inputEmailEl.value = '';
+  inputMessageEl.value = '';
+}
+
+function save(key, value) {
+  try {
+    localStorage.setItem(key, JSON.stringify(value));
+  } catch (err) {
+    console.log(err.name);
+    console.log(err.message);
+  }
+}
+
+function load(key) {
+  try {
+    return localStorage[key] ? JSON.parse(localStorage[key]) : {};
+  } catch (err) {
+    console.log(err.name);
+    console.log(err.message);
+  }
+}
