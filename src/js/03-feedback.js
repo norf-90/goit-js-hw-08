c013f2de5a634b9c99a91b442efe666af4976ffe
Oleksandr Shmatko <blacknorfolk@gmail.com>
2022-12-27 17:48:06 +0200

    fix form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,28 +2,33 @@ import throttle from 'lodash.throttle';
 
 const KEY_NAME = 'feedback-form-state';
 const formEl = document.querySelector('.feedback-form');
-const inputEmailEl = document.querySelector('[name=email]');
-const inputMessageEl = document.querySelector('[name=message]');
-const buttonEl = document.querySelector('[type=submit]');
-const formData = {};
+let formData = load(KEY_NAME) ?? {};
+console.log('Initial formData: ', formData);
 
-inputEmailEl.value = load(KEY_NAME).email ?? '';
-inputMessageEl.value = load(KEY_NAME).message ?? '';
-
+initForm();
 formEl.addEventListener('input', throttle(onFormInput, 500));
-buttonEl.addEventListener('click', onButtonClick);
+formEl.addEventListener('submit', onFormSubmit);
 
 function onFormInput(event) {
   formData[event.target.name] = event.target.value;
+  console.table(formData);
   save(KEY_NAME, formData);
 }
 
-function onButtonClick(event) {
+function initForm() {
+  Object.keys(formData).forEach(key => {
+    formEl.elements[key].value = formData[key];
+    console.log(key);
+  });
+}
+
+function onFormSubmit(event) {
   event.preventDefault();
   console.log(load(KEY_NAME));
   localStorage.removeItem(KEY_NAME);
-  inputEmailEl.value = '';
-  inputMessageEl.value = '';
+  formEl.elements.email.value = '';
+  formEl.elements.message.value = '';
+  formData = {};
 }
 
 function save(key, value) {
@@ -37,7 +42,7 @@ function save(key, value) {
 
 function load(key) {
   try {
-    return localStorage[key] ? JSON.parse(localStorage[key]) : {};
+    return JSON.parse(localStorage[key]);
   } catch (err) {
     console.log(err.name);
     console.log(err.message);
